ffec0bef2bb161d5ebab089be843df362508c4fa
Jonathan Weeks <jonathan.weeks@pmi.com>
2025-03-02 00:48:35 +0100

    Going to sleep

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-# import time
-# import app.data.data_loader as data_loader
-# from app.views.parent.parent_view import ParentView
-# from app.ui.group_selecter import GroupSelecter
-# import app.data.app_data as app_data
-# from app.controllers.group_chooser_c import GroupChooserControler
-from app.aui.specific.student_chooser.student_chooser_c import StudentChooserControler
+import sys
+import importlib
+import app.data.app_data as app_data
+import app.data.loader as loader
+import app.apputil.util as util
 
 def run():
-	gcc = StudentChooserControler()
-	print('finished')
+	menu_filepath = util.get_parent_path() + 'metadata/menu.csv'
+	menu_df = loader.open_df(menu_filepath)
+	while app_data.selected_group != 'exit':
+		module_string = menu_df.loc[menu_df['menu'] == app_data.current_ui, 'controller'].item()
+		if module_string is None:
+			sys.exit()
+		module = None
+		try:
+			module = importlib.import_module(module_string)
+		except:
+			sys.exit()
+		module.Controller()
 run()
--- app/data/app_data.py
+++ app/data/app_data.py
@@ -1,36 +1,11 @@
 import pandas as pd
 import app.apputil.util as util
-# from app.ui_old.record_attendance import RecordAttendance
-# from app.ui_old.group_selecter import GroupSelecter
-# from app.ui_old.class_title_editor import ClassTitleEditor
-# from app.ui_old.date_selecter import DateSelecter
-# from app.ui_old.export_spreadsheet import ExportSpreadsheet
-# from app.ui_old.export_student_attendance import ExportStudentAttendance
-# from app.ui_old.edit_students import EditStudent
-# from app.ui_old.backup_data import BackupData
-# from app.ui_old.quit_app import QuitApp
-# from app.ui_old.menu import Menu
 
-class AppData:
-    def __init__(self, parent_view):
-        self.groups: list[str] = None
-        self.selected_group_index: int = -1
-        self.selected_group: str = 'No group selected'
-        self.selected_date = util.get_today()
-        self.students_df: pd.DataFrame = None
-        self.class_df: pd.DataFrame = None
-        self.sc_object = None
-        self.menu = {
-        		# 'Select a different group': GroupSelecter(self),
-        		# 'Record attendance': RecordAttendance(self),
-        		# 'Change date': DateSelecter(self),
-        		# 'Record class title': ClassTitleEditor(self),
-        		# 'Export spreadsheet': ExportSpreadsheet(self),
-        		# 'Export student attendance': ExportStudentAttendance(self),
-        		# 'Edit students': EditStudent(self),
-                # 'Backup data': BackupData(self),
-        		# 'Quit app': QuitApp(self),
-        		# 'Menu': Menu(self)
-        	}
-        self.current_ui: str = 'Select a different group'
-        self.waiting = False
+groups: list[str] = None
+selected_group_index: int = -1
+selected_group: str = 'No group selected'
+selected_date = util.get_today()
+students_df: pd.DataFrame = None
+class_df: pd.DataFrame = None
+current_ui: str = 'Select a different group'
+waiting = False
